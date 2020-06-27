3bbdd4e1697805f3351a03180fbbdfa39ec1ca99
Shota <shotikotchelidze@gmail.com>
2020-06-27 04:02:39 +0400

    create admin view tamplate

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,4 +21,8 @@ Route::auth();
 
 Route::get('/home', 'HomeController@index');
 
+Route::get('/admin', function(){
+    return view('admin.index');
+});
+
 Route::resource('admin/users', 'AdminUsersController');
